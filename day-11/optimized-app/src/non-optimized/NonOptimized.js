a7d4260105138960f98b63b779a5ea9aa6f21037
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-28 09:01:44 +0530

    query : explanation content loaders

--- day-11/optimized-app/src/non-optimized/NonOptimized.js
+++ day-11/optimized-app/src/non-optimized/NonOptimized.js
@@ -4,57 +4,62 @@ import { useCharacter } from '../custom-hook/use-character-non-optimized'
 export function NonOptimized() {
 
     const [cnt, setCnt] = useState(0)
-    const [ cart, setCart ] = useState(0) 
+    const [cart, setCart] = useState(0)
 
     //https://hp-api.herokuapp.com/api/characters
     //https://acnhapi.com/v1/houseware/
-    const { characters } = useCharacter('https://hp-api.herokuapp.com/api/characters')
-    
-    const onClk = () => setCnt( c => c + 1  )
-    const addPosterToCart = useCallback(() => setCart( cr => cr + 1 ), [characters])
+    const { characters, pulling } = useCharacter('https://hp-api.herokuapp.com/api/characters')
 
-    const findContains = useMemo( () => characters.map( ch => ch['name'].includes('a'))[0], [characters])
+    const onClk = () => setCnt(c => c + 1)
+    const addPosterToCart = useCallback(() => setCart(cr => cr + 1), [characters])
 
-    return(
+    const findContains = useMemo(() => characters.map(ch => ch['name'].includes('a'))[0], [characters])
+
+    return (
         <div className='container'>
             <div className='row'>
-                <input type='button' value='Rerender' onClick={onClk}/>
+                <input type='button' value='Rerender' onClick={onClk} />
             </div>
-            <div className='row'>
-             Total Sell Price {findContains}
-            </div>
+            {
+                pulling && <> ( 
+                    <div className='row'>
+                        Total Sell Price {findContains}
+                    </div>
+                )
+                </>
+            }
             <div>
                 <div className='row'>
-                    <MemoiaedList characters={characters} addPosterToCart={addPosterToCart}/>
+                    <MemoiaedList characters={characters} addPosterToCart={addPosterToCart} />
                 </div>
             </div>
         </div>
     )
 }
 
-const MemoiaedList = React.memo(function CharacterList({ characters , addPosterToCart}) {
-    useEffect( () => {
+const MemoiaedList = React.memo(function CharacterList({ characters, addPosterToCart }) {
+    useEffect(() => {
         console.count('From Huge List')
     })
     return characters.map(
         (chr, inx) => (
-            <Character key={inx} {...chr} addPosterToCart={addPosterToCart}/>
-        ) 
+            <Character key={inx} {...chr} addPosterToCart={addPosterToCart} />
+        )
     )
 })
 
 function Character({ name, image, dob, addPosterToCart }) {
-    useEffect( () => {
+    useEffect(() => {
         console.count('From Character Item')
     })
 
     return (
-        <div className="card m-3" style={{ width: '18rem'}}>
-            <img src={image} className="card-img-top" alt={name}/>
+        <div className="card m-3" style={{ width: '18rem' }}>
+            <img src={image} className="card-img-top" alt={name} />
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">{dob}</p>
-                <input type='button' className="btn btn-primary" value='Buy Poster' onClick={addPosterToCart}/>
+                <input type='button' className="btn btn-primary" value='Buy Poster' onClick={addPosterToCart} />
             </div>
         </div>
     )
